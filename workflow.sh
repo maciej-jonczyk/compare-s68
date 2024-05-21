edde595798f19a50605f42bdd87e0297574bdb18
maciej-jonczyk <56517984+maciej-jonczyk@users.noreply.github.com>
2024-05-21 09:54:55 +0200

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -193,3 +193,80 @@ java -Xmx100g -jar ~/bin/picard.jar MarkDuplicates \
 --OPTICAL_DUPLICATE_PIXEL_DISTANCE 100 \
 --TAGGING_POLICY All ; done
 
+# 7. Sort by coordinate
+# q30/dedup/ level
+
+for f in *.bam; do \
+  samtools sort -@24 ${f} -o ../../sort4varcall/srt-${f}
+done
+
+# 8. CORRECT read-group SM field (all previous files have bad value)
+# sort4varcall level
+
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.16_p1.bam O=popr-readgroup/16_p1.bam RGID=16_p1 RGLB=16_p1 RGPL=illumina RGPU=H3N5LDSXC.1.GTAACATC+NATCGCTG RGSM=16_p1
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.16_p2.bam O=popr-readgroup/16_p2.bam RGID=16_p2 RGLB=16_p2 RGPL=illumina RGPU=H3N5LDSXC.1.CAGCGGTA+NCAAGTCA RGSM=16_p2
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.16_p3.bam O=popr-readgroup/16_p3.bam RGID=16_p3 RGLB=16_p3 RGPL=illumina RGPU=H3N5LDSXC.1.GCCTAGCC+NAGACCAA RGSM=16_p3
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.18_p1.bam O=popr-readgroup/18_p1.bam RGID=18_p1 RGLB=18_p1 RGPL=illumina RGPU=H3N5LDSXC.1.AGGTAAGG+NAATCGTG RGSM=18_p1
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.18_p2.bam O=popr-readgroup/18_p2.bam RGID=18_p2 RGLB=18_p2 RGPL=illumina RGPU=H3N5LDSXC.1.CGCCTTCC+NAGTCCGC RGSM=18_p2
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.18_p3.bam O=popr-readgroup/18_p3.bam RGID=18_p3 RGLB=18_p3 RGPL=illumina RGPU=H3N5LDSXC.1.GACCAGGA+NTAGCCAG RGSM=18_p3
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.19_p1.bam O=popr-readgroup/19_p1.bam RGID=19_p1 RGLB=19_p1 RGPL=illumina RGPU=H3N5LDSXC.1.ACAGGTAT+NTGGTGGC RGSM=19_p1
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.19_p2.bam O=popr-readgroup/19_p2.bam RGID=19_p2 RGLB=19_p2 RGPL=illumina RGPU=H3N5LDSXC.1.CAATAGTC+NTGAGCTG RGSM=19_p2
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.19_p3.bam O=popr-readgroup/19_p3.bam RGID=19_p3 RGLB=19_p3 RGPL=illumina RGPU=H3N5LDSXC.1.CAGCAAGG+NTTAGAGG RGSM=19_p3
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.20_p1.bam O=popr-readgroup/20_p1.bam RGID=20_p1 RGLB=20_p1 RGPL=illumina RGPU=H3N5LDSXC.1.AATGTTCT+NGGTGACT RGSM=20_p1
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.20_p2.bam O=popr-readgroup/20_p2.bam RGID=20_p2 RGLB=20_p2 RGPL=illumina RGPU=H3N5LDSXC.1.ATTATCAA+NAGAGTCG RGSM=20_p2
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.20_p3.bam O=popr-readgroup/20_p3.bam RGID=20_p3 RGLB=20_p3 RGPL=illumina RGPU=H3N5LDSXC.1.CGTTACCA+NAGCGTTC RGSM=20_p3
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.22_p1.bam O=popr-readgroup/22_p1.bam RGID=22_p1 RGLB=22_p1 RGPL=illumina RGPU=H3N5LDSXC.1.TCTGCAAG+NAGGTGAA RGSM=22_p1
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.22_p2.bam O=popr-readgroup/22_p2.bam RGID=22_p2 RGLB=22_p2 RGPL=illumina RGPU=H3N5LDSXC.1.CCAACATT+NAGAGAGC RGSM=22_p2
+java -jar ~/bin/picard.jar AddOrReplaceReadGroups I=srt-dedup-sn-q30.22_p3.bam O=popr-readgroup/22_p3.bam RGID=22_p3 RGLB=22_p3 RGPL=illumina RGPU=H3N5LDSXC.1.TCCAGCAA+NGCCAATT RGSM=22_p3
+
+# 9. Index
+cd popr-readgroup
+
+for f in *.bam ; \
+do \
+  samtools index -@24 ${f} ; \
+done
+
+# 10. Variant-calling
+ulimit -n 20000 # required by freebayes-pararell
+/home/mj/bin/freebayes-scripts/freebayes-parallel <(/home/mj/bin/freebayes-scripts/fasta_generate_regions.py \
+/media/mj/bb71e88b-2122-487c-a602-a2552c680021/NAMv5/Zm-B73-REFERENCE-NAM-5.0.fa.fai 100000) 24 \
+ -f /media/mj/bb71e88b-2122-487c-a602-a2552c680021/NAMv5/Zm-B73-REFERENCE-NAM-5.0.fa 16_p1.bam 16_p2.bam 16_p3.bam 18_p1.bam 18_p2.bam 18_p3.bam 19_p1.bam \
+ 19_p2.bam 19_p3.bam 20_p1.bam 20_p2.bam 20_p3.bam 22_p1.bam 22_p2.bam 22_p3.bam \
+ /media/mj/17d60f37-45c8-4878-8d94-7e95ff7bbddb/map-pl/sort4varcall/srt-hds1-dedup.bam > ../../varcall-parallel/s68.vcf 2>../../varcall-parallel/info
+
+# 11. Quality-filtering
+cd ../../varcall-parallel
+
+vcffilter -f "QUAL > 20 & DP > 5" s68.vcf > s68q20dp5.vcf
+
+# 12. QC
+
+~/bin/bcftools-1.17/bcftools stats -s - s68q20dp5.vcf -F /media/mj/bb71e88b-2122-487c-a602-a2552c680021/NAMv5/Zm-B73-REFERENCE-NAM-5.0.fa > stats20dp5
+
+# multiqc
+multiqc .
+
+# plot
+~/bin/bcftools-1.17/misc/plot-vcfstats -p plot -s stats20dp5
+
+# 13. Annotation
+java -Xmx100G -jar ~/bin/snpEff/snpEff.jar ann -csvStats stat-def.csv -s stat-def.html -c ~/bin/snpEff/snpEff.config Zea_mays -v s68q20dp5.vcf > ann.s68q20dp5.vcf 2>bledy-def
+
+# 14. Multi Bam Summary
+# sort4varcall level
+
+# all files
+multiBamSummary bins --bamfiles \
+srt-dedup-sn-q30.16_p1.bam srt-dedup-sn-q30.16_p2.bam srt-dedup-sn-q30.16_p3.bam srt-dedup-sn-q30.18_p1.bam srt-dedup-sn-q30.18_p2.bam srt-dedup-sn-q30.18_p3.bam srt-dedup-sn-q30.19_p1.bam srt-dedup-sn-q30.19_p2.bam srt-dedup-sn-q30.19_p3.bam srt-dedup-sn-q30.20_p1.bam srt-dedup-sn-q30.20_p2.bam srt-dedup-sn-q30.20_p3.bam srt-dedup-sn-q30.22_p1.bam srt-dedup-sn-q30.22_p2.bam srt-dedup-sn-q30.22_p3.bam /media/mj/17d60f37-45c8-4878-8d94-7e95ff7bbddb/map-pl/sort4varcall/srt-hds1-dedup.bam \
+--labels \
+16_p1 16_p2 16_p3 18_p1 18_p2 18_p3 19_p1 19_p2 19_p3 20_p1 20_p2 20_p3 22_p1 22_p2 22_p3 hds1 \
+-p 24 -o ../pca/s68.npz
+
+# Without old bam (HDS-1 from 2014)
+# popr-readgroup level
+multiBamSummary bins --bamfiles \
+16_p1.bam 16_p2.bam 16_p3.bam 18_p1.bam 18_p2.bam 18_p3.bam 19_p1.bam 19_p2.bam 19_p3.bam 20_p1.bam 20_p2.bam 20_p3.bam 22_p1.bam 22_p2.bam 22_p3.bam \
+--labels \
+16_p1 16_p2 16_p3 18_p1 18_p2 18_p3 19_p1 19_p2 19_p3 20_p1 20_p2 20_p3 22_p1 22_p2 22_p3 \
+-p 24 -o ../../pca/s68-noold.npz
